dd3d045b3fac417c0ef812c20f20d630959a5f90
k33g <ph.charriere@gmail.com>
2011-07-02 22:46:17 +0200

    minified

--- js/species.js
+++ js/species.js
@@ -1,13 +1,15 @@
+/**
+  * species.js - copyright @k33G_org
+  * version 1.0
+  * https://github.com/k33g/species
+  * MIT License
+  */
 
 var Species = (function () {
-    var species = {}
-    /*var species = function(T){
-        return { Class : species.Class }
-    };*/
-    
+    var species = {};
+
     species.Class = function(class_def) {
         var m, k;
-
         if(class_def.Extends) {
             k = Object.create(class_def.Extends);
             if(k.initialize) { k.initialize.prototype = k; }
