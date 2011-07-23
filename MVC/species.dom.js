4ffd00fdd2623bd379f6d340feebdabc2c209acb
k33g <ph.charriere@gmail.com>
2011-07-24 00:09:33 +0200

    no underscore

--- MVC/species.dom.js
+++ MVC/species.dom.js
@@ -2,15 +2,66 @@
 
 var Species = (function (species) {
 
-    species.dom = {
-        findAll : function(selector){ return [].slice.apply(document.querySelectorAll(selector)); },
-        find : function(selector){ return document.querySelector(selector) }
+    species.dom = function(selector) {
+        return {
+            elements : [].slice.apply(document.querySelectorAll(selector)),
+            prop : species.dom.prop,
+            attr : species.dom.attr
+            //attr :
+        }
     };
+    /*
+        $dom('#cmd02').prop('innerHTML')
+        $dom('#cmd02').prop({ innerHTML: 'Hello' });
+        $dom('#cmd02').prop({ onclick : function(){ console.log('click click'); } });
+        $dom('#cmd02').prop({ innerHTML: 'SALUT', onclick : function() { console.log('###SALUT###'); } });
+
+    */
+    species.dom.prop = function(args) {
+        var m;
+        if(typeof args == 'string') {
+            return this.elements.length > 0 ? this.elements[0][args] : null;
+        } else {
+            this.elements.forEach(function(elt) {
+                for(m in args) { elt[m] = args[m]; }
+            });
+            return this;
+        }
+    };
+
+    /*
+        $dom('#cmd02').attr({name:'CMD02', 'data-info' : 'hello'})
+    */
+    species.dom.attr = function(args) {
+        var m;
+        if(typeof args == 'string') {
+            return this.elements.length > 0 ? this.elements[0].getAttribute(args) : null;
+        } else {
+            this.elements.forEach(function(elt) {
+                for(m in args) { elt.setAttribute(m, args[m]); }
+            });
+            return this;
+        }
+
+    };
+
+    /*
+    species.dom.css = function() {
+        var args = arguments;
+        if(this.elements.length > 0 && args.length > 0) {
+            if(args.length > 1) {
+                this.elements.forEach(function(elt) { elt.setAttribute(args[0], args[1]);  });
+                return this;
+            } else {
+                return this.elements[0].getAttribute(args[0]);
+            }
+        } else { return null; }
+    };
+    */
+
+    species.dom.findAll = function(selector){ return [].slice.apply(document.querySelectorAll(selector)); };
+    species.dom.find = function(selector){ return document.querySelector(selector) };
+
     window.$dom = species.dom; /*TODO: tester if exist*/
     return species;
 }(Species));
-
-
-
-
-
