2077a2ff7908c9cc003ad1f3c1c6512bccdb8326
k33g <ph.charriere@gmail.com>
2011-07-31 12:20:28 +0200

    toffee script

---
+++ sandbox/toffee.compiler.js
@@ -0,0 +1,27 @@
+/*--- Compilo ---*/
+console.log("Compiling Toffee Script");
+console.time('Compiling time');
+String.prototype.trim = function() { return this.replace(/^\s+|\s+$/g, ''); };
+
+var src = document.querySelector("script[type='text/toffee']").innerText;
+var bin = '';
+
+var class_list = src.split('Class');
+
+class_list.forEach(function(e) {
+    var class_name = e.split('({')[0].trim();
+    if (class_name !== '') {
+        console.log(' --> compiling ' + class_name + ' ...');
+        src = src.replace('Class '+ class_name, 'var ' + class_name + ' = Species.Class' );
+    }
+
+});
+console.timeEnd('Compiling time');
+
+console.log("Executing Toffee Script");
+
+console.time('Executing time');
+
+document.write('<script>'+src+'<\/script>');
+
+console.timeEnd('Executing time');
