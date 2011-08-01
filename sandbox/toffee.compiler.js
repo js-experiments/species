63eeb051483f9f48ea5bf751b6790b0455b593c0
k33g <ph.charriere@gmail.com>
2011-08-01 06:20:26 +0200

    toffee script

--- sandbox/toffee.compiler.js
+++ sandbox/toffee.compiler.js
@@ -1,30 +1,61 @@
 /*--- Compilo ---*/
+
+/*-----------------------------------------*/
 console.log("Compiling Toffee Script");
-console.time('Compiling time');
-String.prototype.trim = function() { return this.replace(/^\s+|\s+$/g, ''); };
+console.time('Total compiling time');
+/*-----------------------------------------*/
+    String.prototype.trim = function() { return this.replace(/^\s+|\s+$/g, ''); };
 
-var src = document.querySelector("script[type='text/toffee']").innerText;
-var bin = '';
+    var src = document.querySelector("script[type='text/toffee']").innerText;
+    var bin = '';
 
-var class_list = src.split('Class');
+    /*-----------------------------------------*/
+    console.log('1- Class compiling')
+    console.time('Class compiling time');
+    /*-----------------------------------------*/
 
-class_list.forEach(function(e) {
-    var class_name = e.split('({')[0].trim();
-    if (class_name !== '') {
-        console.log(' --> compiling ' + class_name + ' ...');
-        src = src.replace('Class '+ class_name, 'var ' + class_name + ' = Species.Class' );
+        var class_list = src.split('Class');
 
-        //src = src.replace(class_name + ' : function(', 'initialize : function _' + class_name + '(' );
-        src = src.replace(class_name + '(', 'initialize : function _' + class_name + '(' );
-    }
+        class_list.forEach(function(e) {
+            var class_name = e.split('({')[0].trim();
 
-});
-console.timeEnd('Compiling time');
+            if(class_name[0] !== '/') {
+                if (class_name !== '') {
+                    console.log(' --> compiling ' + class_name + ' ...');
+
+                    src = src.replace('Class '+ class_name, 'var ' + class_name + ' = Species.Class' );
+
+                    //src = src.replace(class_name + ' : function(', 'initialize : function _' + class_name + '(' );
+                    src = src.replace(class_name + '(', 'initialize : function _' + class_name + '(' );
+                    src = src.replace('::', 'Extends : ');
+                }
+            }
+
+        });
+    console.timeEnd('Class compiling time');
+
+    /*-----------------------------------------*/
+    console.log('2- System compiling')
+    console.time('System compiling time');
+    /*-----------------------------------------*/
+
+        src = src.replace(/puts\(/g, 'console.log(');
+        src = src.replace(/watch\(/g, 'Species.watch(');
+        src = src.replace(/aop.before\(/g, 'Species.aop.before(');
+        src = src.replace(/aop.after\(/g, 'Species.aop.after(');
+
+    console.timeEnd('System compiling time');
+
+console.timeEnd('Total compiling time');
+
+
 
 console.log("Executing Toffee Script");
 
 console.time('Executing time');
 
+//<script src="species.min.js"></script>
+document.write('<script src="toffee.runtime.js"><\/script>');
 document.write('<script>'+src+'<\/script>');
 
 console.timeEnd('Executing time');
